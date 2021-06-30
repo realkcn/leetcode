fc6dc37fa367fbb9b93582f19079a2215bb37903
Zhou Lin <kcn@kcn.cn>
2021-06-30 23:55:55 +0800

    leetcode 0010

--- leetcode/0010.regular-expression-matching/0010.go
+++ leetcode/0010.regular-expression-matching/0010.go
@@ -93,6 +93,7 @@ type nodeSet []*DFANode
 func isMatch(s string, p string) bool {
 	compile(p)
 
+	//currentNodeSet和nextNodeSet循环使用两个集合，优化一下
 	var dataNodeSet [2]*nodeSet
 	dataNodeSet[0] = new(nodeSet)
 	dataNodeSet[1] = new(nodeSet)
@@ -107,6 +108,7 @@ func isMatch(s string, p string) bool {
 				return true
 			}
 		}
+		//currentNodeSet和nextNodeSet循环互换
 		var tmp *nodeSet
 		tmp = currentNodeSet
 		currentNodeSet = nextNodeSet
